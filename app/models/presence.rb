209aaccb752c524bd0fc24062900846d1f188f0a
Manuel Gonzalez Noriega <manuel@simplelogica.net>
2010-06-11 09:07:14 +0200

    Cancel prior presences

--- app/models/presence.rb
+++ app/models/presence.rb
@@ -6,8 +6,16 @@ class Presence < ActiveRecord::Base
   named_scope :active, :conditions => 'finished_at IS NULL'
   named_scope :finished, :conditions => 'finished_at IS NOT NULL'
   
+  before_create :cancel_prior!
+  
   def finish!
     self.update_attribute(:finished_at, Time.now)
+  end
+
+  private
+  
+  def cancel_prior!
+    ActiveRecord::Base.connection.execute("UPDATE PRESENCES SET finished_at = '#{Time.now.to_s(:db)}' WHERE user_id = #{user.id} AND finished_at IS NULL")
   end
 
 end
