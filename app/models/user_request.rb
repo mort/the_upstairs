17e732c57bbfd2b16a5da380d1579bafa4915cf8
Manuel Gonzalez Noriega <manuel@simplelogica.net>
2010-07-27 07:40:51 +0200

    Little fixes

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -1,11 +1,12 @@
 class UserRequest < ActiveRecord::Base
 
-  STATUS = {:pending => 0, :accepted => 1, :declined => 2}
+  STATUSES = {:pending => 0, :accepted => 1, :declined => 2}
   
-  STATUS.each do |status, value|
+  STATUSES.each do |status, value|
     self.send(:named_scope, status, :conditions => "status = #{value}")
   end
   
+  TYPES = %(Handshake Engagement)
   
   belongs_to :user
   
@@ -14,23 +15,24 @@ class UserRequest < ActiveRecord::Base
   def validate
     errors.add("Not in tile") and return unless user.in_same_tile_that?(requester)
     errors.add("Not in venue") and return unless user.in_same_venue_that?(requester)  
-    errors.add("Unknown request type") and return unless %(Handshake Engagement).include?(type)
+    errors.add("Unknown request type") and return unless TYPES.include?(type)
     errors.add("Not engaged") and return if requires_engagement?(type) && !user.engaged_with?(requester)
   end
   
   def requires_engagement?(type)
     type != 'Engagement'
   end
+  
 
   def accept!
-    update_attributes(:status => STATUS[:accepted], :accepted_at => Time.now)
+    update_attributes(:status => STATUSES[:accepted], :accepted_at => Time.now)
     # a little metaprogramming to invoke the proceed callback on the appropiate class
     # Engagement.proceed, Handshake.proceed, etc.
     klass_callback(:proceed)
   end
 
   def decline!
-    update_attributes(:status => STATUS[:declined], :declined_at => Time.now)
+    update_attributes(:status => STATUSES[:declined], :declined_at => Time.now)
     # a little metaprogramming to invoke the cancel callback on the appropiate class
     # Engagement.cancel, Handshake.cancel, etc.
     klass_callback(:cancel)
@@ -54,8 +56,6 @@ class UserRequest < ActiveRecord::Base
 
     actions.to_json
   end
-  
-  
   
 
 end
